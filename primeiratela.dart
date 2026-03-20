20d2d4b20078abb86f08b40d7bc7ad6feea640cf
Carla Jhenifer Morais Vieira <89932530+carlajhenifermorais@users.noreply.github.com>
2026-03-20 08:24:59 -0300

    Create primeiratela.dart

---
+++ primeiratela.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      home: Scaffold( //estrutura básica tela
+        body: Center(
+          child: Text('Olá, Flutter!'),
+        ),
+      ),
+    ),
+  );
+}
