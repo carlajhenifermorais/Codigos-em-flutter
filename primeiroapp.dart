366664d8daaa01202d34ede271a54d7c9cd35fdc
Carla Jhenifer Morais Vieira <89932530+carlajhenifermorais@users.noreply.github.com>
2026-03-20 08:54:05 -0300

    Create primeiroapp.dart

---
+++ primeiroapp.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    MaterialApp());
+}
+      
+ class Myapp extends StatelessWidget{ //página estatica
+   @override
+   Widget build(BuildContext context){ //constroi e retorna interface
+     return MaterialApp(
+      home: Scaffold( //estrutura básica tela
+        appBar: AppBar( //barra superior
+          title: Text('Meu app'), //titulo pra barra superior
+        ),
+        body: Center(
+          child: Text(
+            'Olá, Flutter!',
+            style: TextStyle(fontSize:32), //estilizar tamanho da fonte
+        ),
+      ),
+     ),
+   );
+  }
+}
