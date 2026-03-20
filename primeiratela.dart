744438da29b2d097849d71feab1158dc27ffc3c8
Carla Jhenifer Morais Vieira <89932530+carlajhenifermorais@users.noreply.github.com>
2026-03-20 08:28:17 -0300

    Update primeiratela.dart

--- primeiratela.dart
+++ primeiratela.dart
@@ -4,6 +4,9 @@ void main(){
   runApp(
     MaterialApp(
       home: Scaffold( //estrutura básica tela
+        appBar: AppBar( //barra superior
+          title: Text('Meu app'), //titulo pra barra superior
+        ),
         body: Center(
           child: Text('Olá, Flutter!'),
         ),
