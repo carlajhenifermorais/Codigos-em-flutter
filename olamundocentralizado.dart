74b2f8a4cbaea2a86b9ab38b34cd4db818fc0d1f
Carla Jhenifer Morais Vieira <89932530+carlajhenifermorais@users.noreply.github.com>
2026-03-20 08:19:01 -0300

    Create olamundocentralizado.dart

---
+++ olamundocentralizado.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart'; //pacote de componentes
+
+void main(){
+  runApp(
+    MaterialApp(
+      home: Center( //para centralizar
+        child: Text('Olá, Flutter!'), //filho do componente visual
+      ),
+    ),
+  );
+}
