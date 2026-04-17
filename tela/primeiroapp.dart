187645c19bead239a19a6dd8424dcf82c0963271
Carla Jhenifer Morais Vieira <89932530+carlajhenifermorais@users.noreply.github.com>
2026-04-17 09:04:22 -0300

    Refactor app to include counter functionality

--- tela/primeiroapp.dart
+++ tela/primeiroapp.dart
@@ -1,26 +1,95 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(
-    MyApp());
+void main() {
+  runApp(MyApp());
 }
-      
- class MyApp extends StatelessWidget{ //página estatica
-   @override
-   Widget build(BuildContext context){ //constroi e retorna interface
-     return MaterialApp(
-      home: Scaffold( //estrutura básica tela
-        appBar: AppBar( //barra superior
-          title: Text('Meu app'), //titulo pra barra superior
-        ),
-        backgroundColor: Colors.pink[100], //mudar fundo
-        body: Center(
-          child: Text(
-            'Oiê Giovanni!!!!',
-            style: TextStyle(fontSize:32), //estilizar tamanho da fonte
-          ),
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TelaContador(),
+    );
+  }
+}
+
+class TelaContador extends StatefulWidget{
+  @override
+  State<TelaContador> createState() => _TelaContadorState();
+}
+
+class _TelaContadorState extends State<TelaContador> {
+  int valor = 0;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("App contador"),
+        backgroundColor: Colors.pink[100],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Valor: $valor",
+                style: TextStyle(fontSize:40)
+            ),
+            
+            SizedBox(
+                width: 20,
+                height: 40,
+            ),
+            
+            ElevatedButton(
+              onPressed: () {
+                setState((){
+                  valor = valor + 1;
+                }); 
+              },
+              child: Text(
+                "Somar",
+                style: TextStyle(fontSize:32)
+              ),
+            ),
+            
+            SizedBox(
+                width: 20,
+                height: 40,
+             ),
+            
+            ElevatedButton(
+              onPressed: () {
+                setState((){
+                  valor = valor - 1;
+                }); 
+              },
+              child: Text(
+                "Subtrair",
+                style: TextStyle(fontSize:32)
+              ),
+            ),
+            
+            SizedBox(
+                width: 20,
+                height: 40,
+            ),
+            
+            ElevatedButton(
+              onPressed: () {
+                setState((){
+                  valor = 0;
+                }); 
+              },
+              child: Text(
+                "Zerar",
+                style: TextStyle(fontSize:32)
+              ),
+            ),
+          ]
         ),
       ),
     );
   }
- }
+}
