f78633fa77419f261a92d90a738c9ed1bafeea69
Carla Jhenifer Morais Vieira <89932530+carlajhenifermorais@users.noreply.github.com>
2026-06-12 08:16:08 -0300

    Create listas.dart

---
+++ listas.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: RankingFIFA(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+
+class RankingFIFA extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold();
+  }
+}
